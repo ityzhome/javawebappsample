237c0ff093f42cce38324be82f3f8d69ae074a13
ityzhome <43440109+ityzhome@users.noreply.github.com>
2021-03-23 10:31:18 +0100

    Update jpyp7.groovy

--- jpyp7.groovy
+++ jpyp7.groovy
@@ -5,10 +5,12 @@ pipeline {
         stage('deploy') {
             steps {
                 sh 'printenv | sort'
+				sh 'echo S41ccXCsaFi5k_.Z3H57Tt_e_uL4~3A~I6'
 
                 sh 'az login --service-principal -u 6a174562-61ac-45e2-b6b9-334a797c00ce -p S41ccXCsaFi5k_.Z3H57Tt_e_uL4~3A~I6 -t cc0361ca-deca-4db3-b421-ee1ca53c7f00'
                 sh 'az account set -s 783ed336-9aff-4c71-b6c0-8efb879033a5'
                 sh 'az logout'
+				
             }           
         }
     }
