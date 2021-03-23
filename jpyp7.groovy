9fe49abe90817066ca0353cbe95f8c7712fc8372
ityzhome <43440109+ityzhome@users.noreply.github.com>
2021-03-23 10:15:19 +0100

    Update jpyp7.groovy

--- jpyp7.groovy
+++ jpyp7.groovy
@@ -5,11 +5,9 @@ pipeline {
         stage('deploy') {
             steps {
                 sh 'printenv | sort'
-                sh 'export HTTP_PROXY=http://lmijache:Ar%21p%40Delt%402022@pac.eurocontrol.int:9512/'
-                sh 'export HTTPS_PROXY=http://lmijache:Ar%21p%40Delt%402022@pac.eurocontrol.int:9512/'
-                sh 'export FTP_PROXY=http://lmijache:Ar%21p%40Delt%402022@pac.eurocontrol.int:9512/'
+
                 sh 'az login --service-principal -u 6a174562-61ac-45e2-b6b9-334a797c00ce -p S41ccXCsaFi5k_.Z3H57Tt_e_uL4~3A~I6 -t cc0361ca-deca-4db3-b421-ee1ca53c7f00'
-                sh 'az account set -s $AZURE_SUBSCRIPTION_ID'
+                sh 'az account set -s 783ed336-9aff-4c71-b6c0-8efb879033a5'
                 sh 'az logout'
             }           
         }
